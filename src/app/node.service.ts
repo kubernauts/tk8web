d7c5a782c5a78063f33f871a542672fdb49791b6
sfbq0o7 <mm@kubernauts.de>
2020-01-06 16:43:34 +0100

    changes

--- src/app/node.service.ts
+++ src/app/node.service.ts
@@ -1,7 +1,6 @@
 import { Clusternode } from './clusternode';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as nodeInfo from '../assets/node.json'
 
 @Injectable({
   providedIn: 'root'
@@ -13,19 +12,7 @@ export class NodeService {
   constructor(private httpClient: HttpClient) { }
 
   getNodes() {
-    nodeInfo.default.items.forEach( ni => {
-      console.log(ni);
-      const node = new Clusternode(ni.metadata.name,ni.kind);
-      node.isMaster=ni.metadata.labels['node-role.kubernetes.io/controlplane'];
-      node.isWorker=ni.metadata.labels['node-role.kubernetes.io/worker'];
-      node.isEtcd=ni.metadata.labels['node-role.kubernetes.io/etcd'];
-      node.Architecture = ni.status.nodeInfo.architecture;
-      node.ContainerRuntime = ni.status.nodeInfo.containerRuntimeVersion;
-      node.KubeVersion = ni.status.nodeInfo.kubeletVersion;
-      node.OS = ni.status.nodeInfo.osImage;
-      node.IpAddress = ni.status.addresses;
-      this.nodes[node.Name] = node;
-    });
+
 
     console.log(this.nodes);
     return this.nodes;
